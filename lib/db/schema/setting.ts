f8781f7f6211cdb33d3be11856122bfb049a2056
oaguinaga <omar.aguinaga94@gmail.com>
2025-08-02 19:33:48 -0600

    feat: initial auth setup (#54)

--- lib/db/schema/setting.ts
+++ lib/db/schema/setting.ts
@@ -1,9 +1,11 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
+
 export const setting = sqliteTable("setting", {
   id: int().primaryKey({ autoIncrement: true }),
   type: text().notNull(),
-  // userId: text().notNull(),
+  userId: int().notNull().references(() => user.id, { onDelete: "cascade" }),
   notationMode: text().default("letter"), // or 'do-re-mi'
   keyboardLayout: text().default("qwerty"),
   showNoteColors: int({ mode: "boolean" }).default(true),
